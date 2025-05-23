0a0d7c5d86788ae0323e76917eac38d706c4a308
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2025-05-23 17:55:18 +0530

    Time: 0 ms (100%), Space: 22.6 MB (78.13%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,22 +1,37 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums)
+//      {
+//         int c=1; 
+//         int  max=nums[0];  
+//         vector<int>ans;
+//         ans.push_back(nums[0]);
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]!=max)
+//             {
+//                 c++; 
+//                 max=nums[i];  
+//                 ans.push_back(nums[i]);
+//             }
+//         } 
+//         nums.clear(); 
+//         nums=ans;
+//         return c;
+//       }
+// };
+
+
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums)
-     {
-        int c=1; 
-        int  max=nums[0];  
-        vector<int>ans;
-        ans.push_back(nums[0]);
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]!=max)
-            {
-                c++; 
-                max=nums[i];  
-                ans.push_back(nums[i]);
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[j]!=nums[i]){ //Unique element fpund
+                nums[i+1]=nums[j]; 
+                i++;
             }
-        } 
-        nums.clear(); 
-        nums=ans;
-        return c;
-      }
+        }
+        return i+1;
+    }
 };
