9a82abbcd89797142635c97abae09dd82b2885e2
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2025-05-24 14:31:37 +0530

    Time: 5 ms (71.7%), Space: 11.4 MB (65.58%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {  
+        unordered_map<char, int> freqT;
+        int required = t.size(), formed = 0;
+        int minLength = INT_MAX, LeftIndex = 0;
+        int l = 0;
+
+        for (char c : t) freqT[c]++;
+
+        for (int r = 0; r < s.size(); r++) {
+            if (freqT[s[r]]-- > 0) formed++;
+
+            while (formed == required) {  
+                if (r - l + 1 < minLength) {
+                    minLength = r - l + 1;
+                    LeftIndex = l;
+                }
+
+                if (++freqT[s[l]] > 0){
+                     formed--;
+                }
+                l++;
+            }
+        }
+        return minLength == INT_MAX ? "" : s.substr(LeftIndex, minLength);
+    }
+};
