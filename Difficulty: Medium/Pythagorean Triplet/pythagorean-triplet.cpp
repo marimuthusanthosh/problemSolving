61778f55670fa3898661d45611dd2cccaead8485
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2025-05-25 22:39:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    bool pythagoreanTriplet(vector<int>& arr) {
+        // code here
+        set<int>st;
+        
+        for( int num : arr ){
+            st.insert(num*num);
+        }
+        
+        for( int num1 : st ){
+            for( int num2 : st ){
+                if(num2 != num1){
+                    if(st.count(num1+num2)) return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+};
