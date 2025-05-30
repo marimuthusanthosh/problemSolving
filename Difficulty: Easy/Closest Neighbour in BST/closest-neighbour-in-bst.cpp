03fe5384ea95f7a90b0d3d2dd022583b85ba15c9
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2025-05-30 22:45:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Closest Neighbour in BST/closest-neighbour-in-bst.cpp
@@ -0,0 +1,33 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+  
+      int findMaxFork(Node* root, int k) {
+        int ans = -1; 
+        while (root != NULL) {
+            if (root->data == k) {
+                return k;
+            }
+            if (root->data < k) {
+                ans = root->data;
+                root = root->right;
+            } else {
+                root = root->left;
+            }
+        }
+        return ans;
+    }
+};
