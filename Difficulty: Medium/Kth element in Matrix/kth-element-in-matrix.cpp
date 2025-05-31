607fee007e4809807e935fd16542ebbb820a0845
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2025-05-31 21:20:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth element in Matrix/kth-element-in-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int kthSmallest(vector<vector<int>> &matrix, int k) {
+        // code here
+        
+        priority_queue<int,vector<int>,greater<>>pq;
+        
+        for(auto i:matrix)
+        {
+            for(auto j:i)
+            {
+                pq.push(j);
+            }
+        }
+        while(k-- >1)
+        {
+            pq.pop();
+        }
+        return pq.top();
+        
+         
+        
+        
+    }
+};
