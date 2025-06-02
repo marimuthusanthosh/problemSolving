dc6d2c1cc4fb290a658ac2bb6946e6e5f9fa589c
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2025-06-02 20:06:15 +0530

    Time: 3 ms (83.48%), Space: 11.5 MB (44.31%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     string minWindow(string s, string t) {  
         unordered_map<char, int> freqT;
-        int required = t.size(), formed = 0;
+        int required = t.size(), formed = 0;         
         int minLength = INT_MAX, LeftIndex = 0;
         int l = 0;
 
         for (char c : t) freqT[c]++;
 
         for (int r = 0; r < s.size(); r++) {
+            
             if (freqT[s[r]]-- > 0) formed++;
 
             while (formed == required) {  
