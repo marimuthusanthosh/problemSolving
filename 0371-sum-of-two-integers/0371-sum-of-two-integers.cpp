96633ba1e2ad270608e30a29dbab0b77e2dc16d5
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2026-02-17 21:37:08 +0530

    Time: 0 ms (100%), Space: 7.8 MB (32.88%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
-    int getSum(int a, int b)  
-    {
-    return a+b;
+    int getSum(int a, int b) {
+
+        while(b!=0){
+            int carry=(a&b)<<1; 
+            cout<<carry;
+            a=a^b; 
+            b=carry;
+        }
+        return a;
+        
     }
 };
