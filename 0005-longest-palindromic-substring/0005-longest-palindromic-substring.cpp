8a96fc2b1266d8d4135efdf771fdd3f47535e7a1
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2026-02-22 12:38:39 +0530

    Time: 696 ms (11.24%), Space: 291.8 MB (8.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -33,7 +33,34 @@ public:
         vector<vector<int>> dp(n, vector<int>(n, -1));
         int start = 0, maxLength = 1;
         f(0, n - 1, s, start, maxLength, dp);
+
+     
         return s.substr(start, maxLength);
         
     }
 };
+
+
+// class Solution {
+// public:
+//     string ans = "";
+//     void expand(string &s , int left ,int right)
+//     {
+//         while(left >= 0 &&  right < s.size())
+//         {
+//             if(s[left] != s[right])
+//                 break;
+//             left--,right++;
+//         }
+//         if(ans.size() < right - left )
+//             ans = s.substr(left + 1 , right - left - 1);
+//     }
+//     string longestPalindrome(string s) {
+//         for(int i = 0 ; i < s.size() ; i++)
+//         {
+//             expand(s , i , i);
+//             expand(s , i , i+1);
+//         }
+//         return ans;
+//     }
+// };
