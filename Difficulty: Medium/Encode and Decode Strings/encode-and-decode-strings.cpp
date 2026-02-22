05a7575502aee855c364722b9f583cb11f90f178
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2026-02-22 12:39:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Encode and Decode Strings/encode-and-decode-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    string encode(vector<string>& s) {    
+        
+        for(int i=0;i<s.size();i++){
+            cout<<s[i]<<" ";
+        }
+        return "";
+        // code here
+    }
+
+    vector<string> decode(string& s) {
+        // code here
+        
+        vector<string>ans;  
+        cout<<s;
+        return ans;
+    }
+};
