4dbbe879b510b441f4bda008ff864b4cc3177576
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2026-02-28 00:59:01 +0530

    Time: 0 ms (100%), Space: 9.4 MB (55.54%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,3 +1,31 @@
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+
+//         if(s.length() != t.length())
+//             return false;
+
+//         map<char, char> mp1;
+//         map<char, char> mp2; 
+
+//         for(int i = 0; i < s.length(); i++) {
+
+//             if(mp1.find(s[i]) == mp1.end() &&
+//                mp2.find(t[i]) == mp2.end()) {
+
+//                 mp1[s[i]] = t[i];
+//                 mp2[t[i]] = s[i];
+//             }
+//             else {
+//                 if(mp1[s[i]] != t[i] || mp2[t[i]] != s[i])
+//                     return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
@@ -5,23 +33,17 @@ public:
         if(s.length() != t.length())
             return false;
 
-        map<char, char> mp1; // s -> t
-        map<char, char> mp2; // t -> s
+        int m1[256] = {0};
+        int m2[256] = {0};
 
         for(int i = 0; i < s.length(); i++) {
 
-            // If mapping not exists
-            if(mp1.find(s[i]) == mp1.end() &&
-               mp2.find(t[i]) == mp2.end()) {
+            if(m1[s[i]] != m2[t[i]])
+                return false;
 
-                mp1[s[i]] = t[i];
-                mp2[t[i]] = s[i];
-            }
-            else {
-                // Check consistency
-                if(mp1[s[i]] != t[i] || mp2[t[i]] != s[i])
-                    return false;
-            }
+            // store i+1 to avoid default 0 confusion
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
         }
 
         return true;
