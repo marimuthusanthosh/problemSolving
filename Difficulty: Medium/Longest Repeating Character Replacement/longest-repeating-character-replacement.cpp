faf9e88b9506641d7350706dc830efba4a6debba
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2026-04-28 17:18:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Repeating Character Replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int longestSubstr(string& s, int k) {
+        // code here
+              int l=0,r=0; 
+        int n=s.length();  
+        map<int,int>freq;
+        int maxf=0;
+        int maxWindowLength=0;
+
+        while(r<n){   
+        
+            freq[s[r]-'A']++;
+
+            maxf=max(maxf,freq[s[r]-'A']); 
+           
+                while(r-l+1-maxf>k){ 
+                    freq[s[l]-'A']--; 
+                    l++;
+                }
+            maxWindowLength=max(maxWindowLength,r-l+1);
+            r++;
+        }
+        return maxWindowLength;     
+    }
+};
