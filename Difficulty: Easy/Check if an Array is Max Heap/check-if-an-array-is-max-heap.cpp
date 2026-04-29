52362c2a6455e8d10c8bff4cb0b4c0bf70b5d1a1
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2026-04-30 01:49:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check if an Array is Max Heap/check-if-an-array-is-max-heap.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    bool isMaxHeap(vector<int> &arr) {
+        // code here
+           int n=arr.size();
+      
+      for(int i=0; i<n; i++){
+        int ch1=2*i+1;
+        int ch2=2*i+2;
+        
+        if(ch1<n && arr[ch1]>arr[i]) return false; 
+        if(ch2<n && arr[ch2]>arr[i]) return false; 
+        
+      }
+      
+      return true;
+    }
+};
