03f5841087e427a6c70f92aac16f45f39b1260c5
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2026-05-12 13:38:08 +0530

    Time: 0 ms (100%), Space: 15.4 MB (54.35%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
 
-    void solve(int num, vector<int>& ans) {
-
-        if (num >= 10) {
-            solve(num / 10, ans);
-        }
-
-        ans.push_back(num % 10);
-    }
-
     vector<int> separateDigits(vector<int>& nums) {
 
         vector<int> ans;
 
         for (int num : nums) {
-            solve(num, ans);
+
+            string s = to_string(num);
+
+            for (char ch : s) {
+                ans.push_back(ch - '0');
+            }
         }
 
         return ans;
