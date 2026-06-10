f81baac9126d2b2783636addb3b55fe089edc464
MSANTHOSH <108514105+marimuthusanthosh@users.noreply.github.com>
2026-06-11 00:44:46 +0530

    Time: 28 ms (5.04%), Space: 103.5 MB (96.9%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -2,17 +2,8 @@ class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
 
-       
-    //  int maxi=INT_MIN,mini=INT_MAX;
-    //   for(int x:nums){
-
-    //     maxi=max(maxi,x);
-    //     mini=min(mini,x);
-    //   }
-    sort(nums.begin(),nums.end());
-    int n=nums.size();
-    return 1LL*k*(nums[n-1]-nums[0]);
-
-        
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        return 1LL*k*(nums[n-1]-nums[0]);      
     }
 };
